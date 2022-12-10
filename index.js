18960436c05914095bbcc0145cf3339cab32f773
mrblanchard <mrjeremyblanchard@gmail.com>
2022-12-10 00:28:35 -0500

    1 file modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Bank Transfers</h1>`;
 function solution(R, V) {
   // Initialize the minimum initial balances for both banks
   let bankA = 0,
